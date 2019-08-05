e3c0af68a8ae0ce2dbf4d87fd49c4b92a1537c1e
0sifr <sbyphone@disroot.org>
2019-08-05 21:45:57 +0100

    third commit

--- qrgap_1.1.py
+++ qrgap_1.1.py
@@ -5,21 +5,33 @@ import os
 FILE_LOCATION = raw_input("Specify directory of text file (eg. /tmp/key.asc): ")
 FILE = open(FILE_LOCATION,"r")
 print "Name of the file: ", FILE.name
+COUNT = 100
 
 qr = 1
 
+
+
+
 while True:
-        TEXT = FILE.read(2000)
-        if TEXT == '': 
+#	COUNT = COUNT - 1
+	FILE.seek(os.SEEK_END)	#start at the end of the file
+        SEG = FILE.read(2000)
+	if SEG == '': 
 		break
-	print TEXT
+	print SEG
 	qr = qrcode.QRCode(
 	    version = None,
 	    error_correction = qrcode.constants.ERROR_CORRECT_L,
 	    box_size = 4,
 	    border = 4,
 		)
-	qr.add_data(TEXT)
+	qr.add_data(SEG)
 	qr.make(fit=True)			#
 	img = qr.make_image()			#
 	img.show()				#shows the made image file
+
+
+
+
+
+
